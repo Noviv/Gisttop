f7509baf739425a55c595669de0bf6b0021972bf
Matthew <matthewwebb65@yahoo.com>
2016-08-16 17:39:34 -0500

    still working on appveyor

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -1,2 +1,5 @@
-dir C:\vcvarsall.bat /s /b
+c:
+cd \
+for /f "delims=" %%a in ('dir C:\vcvarsall.bat /s /b') do set exeLcn=%%a
+%exeLcn%
 cl ../src/main.c /I../libgit2/include /link ../libgit2/build/Debug/git2.lib /OUT:main.exe
