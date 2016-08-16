cc090d4724b9348734e34a2bfe153a627aa47d98
Matthew <matthewwebb65@yahoo.com>
2016-08-16 11:42:48 -0500

    test build

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -1,1 +1,2 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\cl.exe" ../src/main.c /I../libgit2/include /link ../libgit2/build/Debug/git2.lib /OUT:main.exe
+dir C:\vcvarsall.bat /s /b
+cl ../src/main.c /I../libgit2/include /link ../libgit2/build/Debug/git2.lib /OUT:main.exe
