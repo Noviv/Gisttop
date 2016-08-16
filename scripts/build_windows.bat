62a42be43854b9cd7f492aca39bf96b63a5fc62f
Matthew <matthewwebb65@yahoo.com>
2016-08-16 17:43:28 -0500

    again

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -1,5 +1,5 @@
 c:
 cd \
 for /f "delims=" %%a in ('dir C:\vcvarsall.bat /s /b') do set exeLcn=%%a
-%exeLcn%
+"%exeLcn%"
 cl ../src/main.c /I../libgit2/include /link ../libgit2/build/Debug/git2.lib /OUT:main.exe
