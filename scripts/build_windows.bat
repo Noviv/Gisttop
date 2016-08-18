c6107f2508d4c5dbe96f47a5e7b96c782db0e77f
Matthew <matthewwebb65@yahoo.com>
2016-08-18 18:27:14 -0500

    working on windows build system AGAIN

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -4,22 +4,27 @@ if exist E:\Program Files (x86)\Visual Studio\VC\vcvarsall.bat (
 ) else (
 	if exist C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat (
 		call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+		echo Running VS14
 		goto FOUND
 	) else (
 		if exist C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat (
 			call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+			echo Running VS12
 			goto FOUND
 		) else (
 			if exist C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat (
 				call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
+				echo Running VS11
 				goto FOUND
 			) else (
 				if exist C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat (
 					call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+					echo Running VS10
 					goto FOUND
 				) else (
 					if exist C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat (
 						call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+						echo Running VS9
 						goto FOUND
 					) else (
 						echo COULD NOT FIND vcvarsall.bat!
@@ -36,6 +41,6 @@ cl ../src/main.c /I../libgit2/include /link ../libgit2/build/Debug/git2.lib /OUT
 copy "..\libgit2\build\Debug\git2.dll" "..\git2.dll"
 
 if not exist ..\main.exe (
-	echo BUILD ERROR
+	echo DID NOT BUILD main.exe
 	thisbuildfailed
 )
