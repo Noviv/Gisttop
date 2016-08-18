287facbd9b4e7b7142a33afeb01c09d24f865415
Noviv <matthewwebb65@yahoo.com>
2016-08-17 20:09:49 -0500

    disabled windows cmd echo

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -1,5 +1,4 @@
-c:
-cd \
+@echo off
 for /f "delims=" %%a in ('dir C:\vcvarsall.bat /s /b') do set exeLcn=%%a
 "%exeLcn%"
 cl ../src/main.c /I../libgit2/include /link ../libgit2/build/Debug/git2.lib /OUT:main.exe
