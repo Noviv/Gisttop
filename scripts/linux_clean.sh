8072964806dcaa0f01a4825bfb579b5b4705fce6
Matthew <matthewwebb65@yahoo.com>
2016-08-18 18:41:25 -0500

    renamed build system & added cleanup command

---
+++ scripts/linux_clean.sh
@@ -0,0 +1,4 @@
+if [ -f "../main" ];
+then
+	rm ../main
+fi
---
+++ scripts/windows_clean.bat
@@ -0,0 +1,11 @@
+if exist "..\main.exe" (
+	del "..\main.exe"
+)
+
+if exist "..\git2.dll" (
+	del "..\git2.dll"
+)
+
+if exist "main.obj" (
+	del "main.obj"
+)
