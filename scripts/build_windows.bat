d44a09acf048d48c6046546cd65232a0d7869528
Matthew Webb <mwebbmwebb@gmail.com>
2016-08-18 17:43:41 -0500

    (WIP) specifying appveyor

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -1,4 +1,2 @@
-@echo off
-for /f "delims=" %%a in ('dir C:\vcvarsall.bat /s /b') do set exeLcn=%%a
-"%exeLcn%"
+dir C:\vcvarsall.bat /s /b
 cl ../src/main.c /I../libgit2/include /link ../libgit2/build/Debug/git2.lib /OUT:main.exe
