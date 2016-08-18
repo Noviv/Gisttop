16cae4dbf1b94e746e076dd60ad24bc69f72b14d
Matthew <matthewwebb65@yahoo.com>
2016-08-18 18:31:17 -0500

    more work

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -1,30 +1,31 @@
 if exist E:\Program Files (x86)\Visual Studio\VC\vcvarsall.bat (
+	echo RUNNING custom VS
 	call "E:\Program Files (x86)\Visual Studio\VC\vcvarsall.bat"
     goto FOUND
 ) else (
 	if exist C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat (
-		call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 		echo Running VS14
+		call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 		goto FOUND
 	) else (
 		if exist C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat (
-			call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 			echo Running VS12
+			call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 			goto FOUND
 		) else (
 			if exist C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat (
-				call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
 				echo Running VS11
+				call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
 				goto FOUND
 			) else (
 				if exist C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat (
-					call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 					echo Running VS10
+					call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 					goto FOUND
 				) else (
 					if exist C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat (
-						call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 						echo Running VS9
+						call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 						goto FOUND
 					) else (
 						echo COULD NOT FIND vcvarsall.bat!
