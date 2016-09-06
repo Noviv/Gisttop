8c2349101c8af305e2e2e1bfd544203db13f2764
Matthew Webb <matthewwebb65@yahoo.com>
2016-09-06 10:10:38 -0500

    windows still broken

--- scripts/windows_build.bat
+++ scripts/windows_build.bat
@@ -39,9 +39,9 @@ if exist "E:\Program Files (x86)\Visual Studio\VC\vcvarsall.bat" (
 
 :FOUND
 echo -- Creating clone to build with Libgit2...
-more +2 ../src/main.c > ../src/main_nlg2.c
+more +2 "../src/main.c" > "../src/main_nlg2.c"
 
-more ../src/main_nlg2.c
+more "../src/main_nlg2.c"
 
 echo -- Building Gisttop with Libgit2...
 cl ../src/main.c /I../libgit2/include /link ../libgit2/build/Debug/git2.lib /OUT:..\main.exe
