ea54824baee1dfe2836784fd7a87dad9ab9fee74
Matthew Webb <matthewwebb65@yahoo.com>
2016-09-07 16:35:18 -0500

    simplified calls & removed libgit2 nonsense

--- src/main.c
+++ src/main.c
@@ -43,21 +43,17 @@ void shutdown();
 
 int main(int argc, char* argv[]) {
 	if (argc < 2) {
-		printf("usage: main [-n] <git directory>\n");
+		printf("usage: main <git directory>\n");
 		printf("\t <git directory> path to monitored git directory\n");
 		return 0;
 	}
 
 	init();
 
-#ifdef USING_LIBGIT2
-	git_libgit2_init();
-#endif
-
-	printf("git repo: %s\n", argv[argc - 1]);
+	printf("Starting Gisttop with directory: %s\n", argv[argc - 1]);
 
 	if (_dir_exist(argv[argc - 1]) == false) {
-		printf("does not exist\n");
+		printf("Directory does not exist!\n");
 		return 0;
 	}
 
@@ -69,15 +65,13 @@ int main(int argc, char* argv[]) {
 	strcat(checkdir, "/.git");
 	if (_dir_exist(checkdir) == false) {
 #endif
-		printf("is not a git repo\n");
+		printf("Directory is not a git repository!\n");
 		return 0;
 	}
 
 	loop(argv[argc - 1]);
 
-#ifdef USING_LIBGIT2
-	git_libgit2_shutdown();
-#endif
+	shutdown();
 
 	return 0;
 }
@@ -100,33 +94,22 @@ bool _dir_exist(const char* path) {
 }
 
 void init() {
-
+#ifdef USING_LIBGIT2
+	git_libgit2_init();
+#endif
 }
 
 void loop(const char* repo_path) {
+	printf("Starting Gisttop notifications...\n");
 #ifdef USING_LIBGIT2
-	int error;
-	git_repository* repo = NULL;
-	git_remote* remote = NULL;
-
-	error = git_repository_open_ext(&repo, repo_path, GIT_REPOSITORY_OPEN_NO_SEARCH, NULL);
-
-	if (error < 0) {
-		const git_error* e = giterr_last();
-		printf("error %d/%d: %s\n", error, e->klass, e->message);
-		return;
-	}
-
-	//do loopy stuff
-	error = git_remote_lookup(&remote, repo, "origin");
-	error = git_remote_fetch(remote, NULL, NULL, NULL);
-
-	//check stuff
-
-	git_repository_free(repo);
+	//TODO Libgit2 implemenentation of notification loop
+#else
+	//TODO system implementation of notification loop
 #endif
 }
 
 void shutdown() {
-
+#ifdef USING_LIBGIT2
+	git_libgit2_shutdown();
+#endif
 }
