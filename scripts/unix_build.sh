7056c0322f6ae0e351052927d7abc8a92587a6fb
Matthew Webb <matthewwebb65@yahoo.com>
2016-09-27 15:08:28 -0500

    (WIP - broken) working on multi-file build system

--- scripts/unix_build.sh
+++ scripts/unix_build.sh
@@ -11,16 +11,16 @@ then
 fi
 
 echo -- Creating clone to build without Libgit2...
-cp ../src/main.c ../src/main_nlg2.c
-touch ../src/main_nlg2.c.new
-tail -n +3 ../src/main_nlg2.c > ../src/main_nlg2.c.new
-mv ../src/main_nlg2.c.new ../src/main_nlg2.c
+cp ../src/c/main.c ../src/c/main_nlg2.c
+touch ../src/c/main_nlg2.c.new
+tail -n +3 ../src/c//main_nlg2.c > ../src/c/main_nlg2.c.new
+mv ../src/c/main_nlg2.c.new ../src/c/main_nlg2.c
 
 echo -- Building Gisttop with Libgit2...
-gcc -Wall -I../libgit2/include ../src/main.c -L../libgit2/build -lgit2 -o ../main
+gcc -Wall -I../libgit2/include -I../src/include/*.h ../src/c/*.c -L../libgit2/build -lgit2 -o ../main
 
 echo -- Building Gisttop without Libgit2...
-gcc -Wall ../src/main_nlg2.c -o ../main_nlg2
+gcc -Wall -I../src/include/*.h ../src/c/main_nlg2.c -o ../main_nlg2
 
 echo -- Cleaning up non-Libgit2 build...
-rm ../src/main_nlg2.c
+rm ../src/c/main_nlg2.c
