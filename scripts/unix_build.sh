02a816d2a5715df64d64108a1e760616b7278b5f
Matthew Webb <matthewwebb65@yahoo.com>
2016-09-30 10:47:46 -0500

    (WIP) working on multi-file build system

--- scripts/unix_build.sh
+++ scripts/unix_build.sh
@@ -10,17 +10,24 @@ then
 	cd scripts
 fi
 
-echo -- Creating clone to build without Libgit2...
-cp ../src/c/main.c ../src/c/main_nlg2.c
-touch ../src/c/main_nlg2.c.new
-tail -n +3 ../src/c//main_nlg2.c > ../src/c/main_nlg2.c.new
-mv ../src/c/main_nlg2.c.new ../src/c/main_nlg2.c
+echo -- Setting up Gisttop build space...
+mkdir ../src/c/nlg2
+
+echo -- Creating clone of main...
+cp ../src/c/main.c ../src/c/nlg2/main.c
+touch ../src/c/nlg2/main.c.new
+tail -n +3 ../src/c/nlg2/main.c > ../src/c/nlg2/main.c.new
+mv ../src/c/nlg2/main.c.new ../src/c/nlg2/main.c
+
+echo -- Creating clone of all other files...
+cp -a ../src/c/*.c ../src/c/nlg2/*.c
 
 echo -- Building Gisttop with Libgit2...
 gcc -Wall -I../libgit2/include -I../src/include/*.h ../src/c/*.c -L../libgit2/build -lgit2 -o ../main
 
 echo -- Building Gisttop without Libgit2...
-gcc -Wall -I../src/include/*.h ../src/c/main_nlg2.c -o ../main_nlg2
+gcc -Wall ../src/c/nlg2/*.c -o ../main_nlg2
 
 echo -- Cleaning up non-Libgit2 build...
-rm ../src/c/main_nlg2.c
+rm -rf ../src/c/nlg2
+rm -rf ../src/include/nlg2
