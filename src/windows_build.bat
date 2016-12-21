c8f5e837c94cbe7c533376046561c8729d488db2
Matthew <matthewwebb65@yahoo.com>
2016-12-20 18:40:50 -0600

    removed useless windows build tag

--- src/windows_build.bat
+++ src/windows_build.bat
@@ -3,32 +3,26 @@
 if exist "E:\Program Files (x86)\Visual Studio\VC\vcvarsall.bat" (
 	echo -- Running custom VS
 	call "E:\Program Files (x86)\Visual Studio\VC\vcvarsall.bat"
-    goto FOUND
 ) else (
 	if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
 		echo -- Running VS14
 		call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-		goto FOUND
 	) else (
 		if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
 			echo -- Running VS12
 			call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
-			goto FOUND
 		) else (
 			if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
 				echo -- Running VS11
 				call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
-				goto FOUND
 			) else (
 				if exist "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
 					echo -- Running VS10
 					call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
-					goto FOUND
 				) else (
 					if exist "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" (
 						echo -- Running VS9
 						call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
-						goto FOUND
 					) else (
 						echo COULD NOT FIND vcvarsall.bat!
 						thisbuildfailed
@@ -39,7 +33,6 @@ if exist "E:\Program Files (x86)\Visual Studio\VC\vcvarsall.bat" (
 	)
 )
 
-:FOUND
 if not exist ../libgit2/build/ (
 	echo -- Building Libgit2...
 	cd ../libgit2
